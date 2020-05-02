d088a5c0f1d8c85afaaa4cc301d26889d2e81129
pavithra <pavithraramesh07@gmail.com>
2020-05-02 20:36:11 +0530

    updatd coded

--- src/Task3.java
+++ src/Task3.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Task3 {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\admin\\eclipse-workspace\\Seleniumm\\Driver\\chromedriver.exe");
+				System.getProperty("user.dir")+"\\Driver\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
@@ -16,10 +16,10 @@ public class Task3 {
 		t2.sendKeys("123456");
 		WebElement t3 = driver.findElement(By.xpath("//input[@value='Log In']"));
 		t3.click();
+		System.out.println("done");
 
 		
 
-		
+	
 	}
-
 }
